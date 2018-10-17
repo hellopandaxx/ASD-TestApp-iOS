2e7a445dfa239bd741a153a1458cde2bf0bf8a36
Sergey Kuznetsov <skuznetsov@skuznetsov-mbp-spb.intermedia-inc.net>
2018-10-17 08:53:13 +0300

    Initial commit

---
+++ ASD-TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ASD-TestApp
+//
+//  Created by Sergey Kuznetsov on 17/10/2018.
+//  Copyright © 2018 Sergey Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
